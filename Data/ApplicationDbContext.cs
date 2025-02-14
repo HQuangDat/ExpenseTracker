1b9825511cc10ccacbd377d8a6c6e14407bb623c
HoQuangDat <hoquangdat123@gmail.com>
2025-02-14 13:51:41 +0700

    Set up ConnectionString and DbContext to able to using SQLServer.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
